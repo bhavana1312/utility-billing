159a2889bc0c5e16a960ac9b1692884891028958
bhavana1312 <bhavana13sree@gmail.com>
2025-12-31 12:51:21 +0530

    create accounts-dashboard page

---
+++ utility-billing-ui/src/app/pages/dashboards/accounts-dashboard/accounts-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accounts-dashboard',
+  imports: [],
+  templateUrl: './accounts-dashboard.html',
+  styleUrl: './accounts-dashboard.css',
+})
+export class AccountsDashboard {
+
+}
