e9dee5875b0254bd5a749bb64ceb33cd536031b5
bhavana1312 <bhavana13sree@gmail.com>
2025-12-31 12:50:51 +0530

    create billing-dashboard page

---
+++ utility-billing-ui/src/app/pages/dashboards/billing-dashboard/billing-dashboard.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-dashboard',
+  imports: [],
+  templateUrl: './billing-dashboard.html',
+  styleUrl: './billing-dashboard.css',
+})
+export class BillingDashboard {
+
+}
